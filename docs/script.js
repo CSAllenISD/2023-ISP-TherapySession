21c0698506e831bd5cdc9857089c84007c9d884c
Hyunseo Lee <hyunseo.lee@student.allenisd.org>
2023-04-29 21:33:50 -0500

    make the function sendEmail()

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,24 @@
-const btn = document.getElementById("submit");
-document.getElementById("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    btn.value = "Submitting...";
-    const serviceID = "service_fv6oze8";
-    const templateID1 = "template_mtyfdys";
-    const templateID2 = "template_m5b91ug";
-    emailjs.sendForm(serviceID, templateID1, this)
-        .then(() => {
-	    btn.value = "Submitted!";
-	}, (err) => {
-	    btn.value = "Failed!";
-	});
-    emailjs.sendForm(serviceID, templateID2, this)
-        .then(() => {
-	    btn.value = "Submitted!";
-	}, (err) => {
-	    btn.value = "Failed!";
-	});
-    document.getElementById("formContainer").style.display = "none";
-    document.getElementById("thankYou").style.display = "block";
-});
+function sendEmail() {
+    const btn = document.getElementById("submit");
+    document.getElementById("form").addEventListener("submit", function(event) {
+	event.preventDefault();
+	btn.value = "Submitting...";
+	const serviceID = "service_fv6oze8";
+	const templateID1 = "template_mtyfdys";
+	const templateID2 = "template_m5b91ug";
+	emailjs.sendForm(serviceID, templateID1, this)
+            .then(() => {
+		btn.value = "Submitted!";
+	    }, (err) => {
+		btn.value = "Failed!";
+	    });
+	emailjs.sendForm(serviceID, templateID2, this)
+            .then(() => {
+		btn.value = "Submitted!";
+	    }, (err) => {
+		btn.value = "Failed!";
+	    });
+	document.getElementById("formContainer").style.display = "none";
+	document.getElementById("thankYou").style.display = "block";
+    });
+}
