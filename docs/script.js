9498aa6245ae30e8e2d0435ffbbdfa69ddee7e2a
Hyunseo Lee <hyunseo.lee@student.allenisd.org>
2023-04-29 23:43:09 -0500

    remove sendEmail() function and onSubmit

--- docs/script.js
+++ docs/script.js
@@ -1,27 +1,25 @@
-function sendEmail() {
-    const btn = document.getElementById("submit");
-    document.getElementById("form").addEventListener("submit", function(event) {
-	event.preventDefault();
-	btn.value = "Submitting...";
-	const serviceID = "service_fv6oze8";
-	const templateID1 = "template_mtyfdys";
-	const templateID2 = "template_m5b91ug";
-	emailjs.sendForm(serviceID, templateID1, this)
-            .then(() => {
-		btn.value = "Submitted!";
-	    }, (err) => {
-		btn.value = "Failed!";
-	    });
-	emailjs.sendForm(serviceID, templateID2, this)
-            .then(() => {
-		btn.value = "Submitted!";
-	    }, (err) => {
-		btn.value = "Failed!";
-	    });
-	document.getElementById("formContainer").style.display = "none";
-	document.getElementById("thankYou").style.display = "block";
-    });
-}
+const btn = document.getElementById("submit");
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    btn.value = "Submitting...";
+    const serviceID = "service_fv6oze8";
+    const templateID1 = "template_mtyfdys";
+    const templateID2 = "template_m5b91ug";
+    emailjs.sendForm(serviceID, templateID1, this)
+        .then(() => {
+	    btn.value = "Submitted!";
+	}, (err) => {
+	    btn.value = "Failed!";
+	});
+    emailjs.sendForm(serviceID, templateID2, this)
+        .then(() => {
+	    btn.value = "Submitted!";
+	}, (err) => {
+	    btn.value = "Failed!";
+	});
+    document.getElementById("formContainer").style.display = "none";
+    document.getElementById("thankYou").style.display = "block";
+});
 $(document).ready(function() {
     $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
 	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
