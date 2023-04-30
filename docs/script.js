b138f14322acd6b9c13942eb41d4d1801d5473b2
Hyunseo Lee <hyunseo.lee@student.allenisd.org>
2023-04-29 19:39:51 -0500

    Make the Send Feedback form on the main page and send email when submitting using EmailJS API

---
+++ docs/script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("submit");
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    btn.value = "Submitting...";
+    const serviceID = "service_fv6oze8";
+    const templateID1 = "template_mtyfdys";
+    const templateID2 = "template_m5b91ug";
+    emailjs.sendForm(serviceID, templateID1, this)
+        .then(() => {
+	    btn.value = "Submitted!";
+	    window.location.href = 'index.html';
+	}, (err) => {
+	    btn.value = "Failed!";
+	});
+    emailjs.sendForm(serviceID, templateID2, this)
+        .then(() => {
+	    btn.value = "Submitted!";
+	}, (err) => {
+	    btn.value = "Failed!";
+	});
+});
